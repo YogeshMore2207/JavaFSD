0c5724554ecbc40fd437bf4bd52456592c9637dc
praga <praga@DESKTOP-NFDAJU0>
2022-09-29 12:35:08 +0530

    First Commit

---
+++ Day1-ConnectingEclipseWithGitHub/src/orgtnsindiabasic/Demo.java
@@ -0,0 +1,10 @@
+package orgtnsindiabasic;
+
+public class Demo {
+	
+	public static void main(String args[]) {
+		int x=12;
+		System.out.println(x);
+	}
+
+}
---
+++ Day1-ConnectingEclipseWithGitHub/src/orgtnsindiabasic/Testingeclipse.java
@@ -0,0 +1,10 @@
+package orgtnsindiabasic;
+//program on to print the statement
+public class Testingeclipse {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Eclipse");
+	}
+	
+
+}
